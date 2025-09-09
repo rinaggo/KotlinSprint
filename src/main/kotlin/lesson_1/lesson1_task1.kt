603b5a22a17c194ab3150eb7c187aefb316f0e4b
rinaggo <198769968+rinaggo@users.noreply.github.com>
2025-09-09 22:33:11 +0600

    KS-1-1 initializing two variables

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,5 +1,8 @@
 package org.example.lesson_1
 
 fun main(){
-    println("Тестовое решение к уроку 1 задаче 1 ")
+
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за покупку!"
+
 }
