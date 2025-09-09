abd76ce9a1e89a4ef2f4c88952bf6b7e9d29b172
rinaggo <198769968+rinaggo@users.noreply.github.com>
2025-09-09 23:05:48 +0600

    KS-1-2 initializing number of employees and the console output of this value

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main(){
+
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(thankYouMessage)
+
+    var numberOfEmployees: Int = 2000
+    //println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+
+}
