87d419307a7657afea093eebe63e34ca2322ec0b
rinaggo <198769968+rinaggo@users.noreply.github.com>
2025-09-11 22:34:09 +0600

    KS-2-3 added a constant and output everything into separate variables

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,18 @@
 package org.example.lesson_2
 
+const val MINUTES_IN_HOUR = 60
+
 fun main(){
 
+    val departureHour= 9
+    val departureMinute = 39
     val travelTimeMinutes = 457
-    val departureTimeMinutes = 9 * 60 + 39
+    val departureTimeMinutes = departureHour * MINUTES_IN_HOUR + departureMinute
 
     val arrivalTimeMinutes = travelTimeMinutes + departureTimeMinutes
 
-    val arrivalHours = arrivalTimeMinutes / 60
-    val arrivalMinutes = arrivalTimeMinutes % 60
+    val arrivalHours = arrivalTimeMinutes / MINUTES_IN_HOUR
+    val arrivalMinutes = arrivalTimeMinutes % MINUTES_IN_HOUR
 
     print(arrivalHours)
     print(":")
