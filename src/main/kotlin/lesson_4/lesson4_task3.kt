fb3a8c00fd664585fa147a96462397b895366d4e
rinaggo <198769968+rinaggo@users.noreply.github.com>
2025-09-16 23:06:11 +0600

    KS-4-3 Make all the criteria of favorable conditions into constants

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_4
 
+const val EXPECTED_SUNNY = true
+const val EXPECTED_AWNING_OPEN = true
+const val REQUIRED_HUMIDITY = 20
+const val FORBIDDEN_SEASON = "зима"
+
 fun main(){
 
     val weatherToday = true
@@ -7,5 +12,5 @@ fun main(){
     val airHumidity = 20
     val timeOfTheYear = "зима"
 
-    println("Благоприятные ли сейчас условия для роста бобовых? ${weatherToday && awningPosition && airHumidity == 20 && timeOfTheYear != "зима"}")
+    println("Благоприятные ли сейчас условия для роста бобовых? ${weatherToday == EXPECTED_SUNNY && awningPosition == EXPECTED_AWNING_OPEN && airHumidity == REQUIRED_HUMIDITY && timeOfTheYear != FORBIDDEN_SEASON}")
 }
