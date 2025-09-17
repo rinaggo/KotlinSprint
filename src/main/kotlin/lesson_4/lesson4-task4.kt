c3eed85da6595f251d53ff890a6d7a68422d0f46
rinaggo <198769968+rinaggo@users.noreply.github.com>
2025-09-17 22:15:39 +0600

    KS-4-4 remove unnecessary variables, leave isHandAndPressDay

--- src/main/kotlin/lesson_4/lesson4-task4.kt
+++ src/main/kotlin/lesson_4/lesson4-task4.kt
@@ -6,15 +6,10 @@ fun main(){
 
     val isHandAndPressDay = dayNumber % 2 != 0
 
-    var hands = isHandAndPressDay
-    var press = isHandAndPressDay
-    var back = !isHandAndPressDay
-    var legs = !isHandAndPressDay
-
     println("""
-        Упражнение для рук:    $hands
-        Упражнение для ног:    $legs
-        Упражнение для спины:  $back
-        Упражнение для пресса: $press
+        Упражнение для рук:    $isHandAndPressDay
+        Упражнение для ног:    ${!isHandAndPressDay}
+        Упражнение для спины:  ${!isHandAndPressDay}
+        Упражнение для пресса: $isHandAndPressDay
     """.trimIndent())
 }
