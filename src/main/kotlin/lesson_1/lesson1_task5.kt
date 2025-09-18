0c7acaffbd577b1d0373ce13846127af9cd1a4b7
rinaggo <198769968+rinaggo@users.noreply.github.com>
2025-09-18 19:22:46 +0600

    KS-1-5 used String.format in the solution

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -10,13 +10,5 @@ fun main(){
     val hour = allSecond / (MINUTES_IN_HOUR * SECONDS_IN_MINUTE)
     val second = allSecond % SECONDS_IN_MINUTE
 
-    if (hour < 10 && second < 10) {
-        println("0$hour:$minute:0$second")
-    } else if (hour > 9 && second < 10)  {
-        println("$hour:$minute:0$second")
-    } else if (second > 9 && hour < 10) {
-        println("0$hour:$minute:$second")
-    } else {
-        println("$hour:$minute:$second")
-    }
+   println(String.format("%02d:%02d:%02d", hour, minute, second))
 }
